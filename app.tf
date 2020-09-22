6ad3ba0282d51357dab36613c46477dc41dffe0a
s3722763 <s3722763@student.rmit.edu.au>
2020-09-22 18:44:43 +1000

    Fixed identifiers and ami used

--- app.tf
+++ app.tf
@@ -3,7 +3,7 @@ resource "aws_key_pair" "deployer" {
   public_key = var.public_key
 }
 
-resource "aws_launch_configuration" "project_frontend" {
+resource "aws_launch_configuration" "project" {
   name            = "web_config"
   image_id        = var.ami_id
   instance_type   = "t2.micro"
@@ -12,24 +12,24 @@ resource "aws_launch_configuration" "project_frontend" {
   key_name = aws_key_pair.deployer.key_name
 }
 
-resource "aws_lb_target_group" "project_frontend" {
+resource "aws_lb_target_group" "project" {
   name     = "todo-app-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 }
 
-resource "aws_autoscaling_group" "project_frontend" {
+resource "aws_autoscaling_group" "project" {
   name                 = "terraform-asg-example"
-  launch_configuration = aws_launch_configuration.project_frontend.name
+  launch_configuration = aws_launch_configuration.project.name
   min_size             = 1
   max_size             = 1
   vpc_zone_identifier  = [aws_subnet.private_az1.id, aws_subnet.private_az2.id, aws_subnet.private_az3.id]
-  target_group_arns    = [aws_lb_target_group.project_frontend.arn]
+  target_group_arns    = [aws_lb_target_group.project.arn]
 }
 
-resource "aws_lb" "project_frontend" {
-  name               = "todo-app-lb"
+resource "aws_lb" "project" {
+  name               = "project-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_http_ssh.id]
@@ -41,13 +41,13 @@ resource "aws_lb" "project_frontend" {
 }
 
 resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.project_frontend.arn
+  load_balancer_arn = aws_lb.project.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.project_frontend.arn
+    target_group_arn = aws_lb_target_group.project.arn
   }
 }
 
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "endpoint" {
-  value = aws_lb.todo_app.dns_name
+  value = aws_lb.project.dns_name
 }
